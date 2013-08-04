484d608b44ac2203598a955534b9c419331f3a4b
HC <hc@vst.io>
2013-08-04 06:12:24 +0200

    Removed .eunit

--- .eunit/checkepp_test.erl
+++
@@ -1,8 +0,0 @@
--module(checkepp_test).
--include_lib("eunit/include/eunit.hrl").
-
-start_test() -> 
-    epp:delme(), 
-    ?assertEqual(1,1).
-%stop_test() -> epp:stop().
-%hans_test() -> ?assertEqual(1,1).
