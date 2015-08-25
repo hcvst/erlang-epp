2d47ed275674452702828fa8393af1a3a8a0f1b3
Dave Cottlehuber <dch@skunkwerks.at>
2015-08-25 22:15:37 +0200

    Become more OTP friendly
    - remove clash with epp OTP application
    - remove rebar bring your own
    - ignore things that should be ignored

--- src/eepp.erl
+++ src/eepp.erl
@@ -1,29 +1,29 @@
--module(epp).
+-module(eepp).
 -compile(export_all).
 %-export([start/0, delme/0]).
--include("epp.hrl").
+-include("eepp.hrl").
 
 
 start() ->
     application:start(sasl),
     ssl:start(),
-    application:start(epp).
+    application:start(eepp).
 
 stop() ->
-    application:stop(epp).
+    application:stop(eepp).
 
 login() ->
     Record = #login{
-        username=epp_config:get(username),
-        password=epp_config:get(password)
+        username=eepp_config:get(username),
+        password=eepp_config:get(password)
         },
-    epp_server:command(login, Record).
-    
+    eepp_server:command(login, Record).
+
 logout() ->
     Record = #logout{},
-    epp_server:command(logout, Record).
+    eepp_server:command(logout, Record).
 
-contact_create(Id, Type, Name, Street, Suburb, City, Province, 
+contact_create(Id, Type, Name, Street, Suburb, City, Province,
     Postcode, CountryCode, Voice, Fax, Email, Password) ->
     Record = #contact_create{
         id=Id,
@@ -40,9 +40,9 @@ contact_create(Id, Type, Name, Street, Suburb, City, Province,
         email=Email,
         password=Password
         },
-    epp_server:command(contact_create, Record).
+    eepp_server:command(contact_create, Record).
 
-contact_update(Id, Type, Name, Street, Suburb, City, Province, 
+contact_update(Id, Type, Name, Street, Suburb, City, Province,
     Postcode, CountryCode, Voice, Fax, Email, Password) ->
     Record = #contact_update{
         id=Id,
@@ -59,30 +59,30 @@ contact_update(Id, Type, Name, Street, Suburb, City, Province,
         email=Email,
         password=Password
         },
-    epp_server:command(contact_update, Record).
+    eepp_server:command(contact_update, Record).
 
 contact_delete(Id) ->
     Record = #contact_delete{id=Id},
-    epp_server:command(contact_delete, Record).
+    eepp_server:command(contact_delete, Record).
 
 contact_info(Id) ->
     contact_info(Id, false, false).
 
-contact_info(Id, IncludeDomainListing, IncludeBalance) 
+contact_info(Id, IncludeDomainListing, IncludeBalance)
     when is_boolean(IncludeDomainListing)
     and is_boolean(IncludeBalance) ->
     Record = #contact_info{
         id=Id,
         includeDomainListing=atom_to_list(IncludeDomainListing),
         includeBalance=atom_to_list(IncludeBalance)},
-    epp_server:command(contact_info, Record).
+    eepp_server:command(contact_info, Record).
 
 domain_check(Name) ->
     Record = #domain_check{name=Name},
-    epp_server:command(domain_check, Record).
+    eepp_server:command(domain_check, Record).
 
-domain_create_delegate(Name, Ns1, Ns2, 
-    RegistrantId, Password, Autorenew) when is_boolean(Autorenew)-> 
+domain_create_delegate(Name, Ns1, Ns2,
+    RegistrantId, Password, Autorenew) when is_boolean(Autorenew)->
     Record = #domain_create_delegate{
         name=Name,
         ns1=Ns1,
@@ -90,7 +90,7 @@ domain_create_delegate(Name, Ns1, Ns2,
         registrantId=RegistrantId,
         password=Password,
         autorenew=atom_to_list(Autorenew)},
-    epp_server:command(domain_create_delegate, Record).
+    eepp_server:command(domain_create_delegate, Record).
 
 domain_create_subordinate(Name, Ns1, Ns1Ip, Ns2, Ns2Ip,
     RegistrantId, Password, Autorenew) when is_boolean(Autorenew) ->
@@ -103,31 +103,31 @@ domain_create_subordinate(Name, Ns1, Ns1Ip, Ns2, Ns2Ip,
         registrantId=RegistrantId,
         password=Password,
         autorenew=atom_to_list(Autorenew)},
-    epp_server:command(domain_create_subordinate,
+    eepp_server:command(domain_create_subordinate,
         Record).
 
 domain_info(Name) ->
     Record = #domain_info{name=Name},
-    epp_server:command(domain_info, Record).
+    eepp_server:command(domain_info, Record).
 
 domain_add_status(Name, Status) ->
     % currently only clientHold supported by registry
     Record = #domain_add_status{
         name=Name,
         status=Status},
-    epp_server:command(domain_add_status, Record).
+    eepp_server:command(domain_add_status, Record).
 
 domain_remove_status(Name, Status) ->
     Record = #domain_remove_status{
         name=Name,
         status=Status},
-    epp_server:command(domain_remove_status, Record).
+    eepp_server:command(domain_remove_status, Record).
 
 domain_change_registrant(Name, RegistrantId) ->
     Record = #domain_change_registrant{
         name=Name,
         registrantId=RegistrantId},
-    epp_server:command(domain_change_registrant, Record).
+    eepp_server:command(domain_change_registrant, Record).
 
 
 domain_add_host_delegate(Name, Ns) ->
@@ -135,7 +135,7 @@ domain_add_host_delegate(Name, Ns) ->
         name=Name,
         ns=Ns
     },
-    epp_server:command(domain_add_host_delegate, Record).
+    eepp_server:command(domain_add_host_delegate, Record).
 
 domain_add_host_subordinate(Name, Ns, NsIp) ->
     Record = #domain_add_host_subordinate{
@@ -143,49 +143,49 @@ domain_add_host_subordinate(Name, Ns, NsIp) ->
         ns=Ns,
         nsIp=NsIp
     },
-    epp_server:command(domain_add_host_subordinate, Record).
+    eepp_server:command(domain_add_host_subordinate, Record).
 
-domain_remove_host(Name, Ns) -> 
+domain_remove_host(Name, Ns) ->
     Record = #domain_remove_host{
         name=Name,
         ns=Ns
     },
-    epp_server:command(domain_remove_host, Record).
+    eepp_server:command(domain_remove_host, Record).
 
 domain_delete(Name) ->
     Record = #domain_delete{name=Name},
-    epp_server:command(domain_delete, Record).
+    eepp_server:command(domain_delete, Record).
 
 domain_renew(Name, CurExpDate) ->
     Record = #domain_renew{name=Name, curExpDate=CurExpDate},
-    epp_server:command(domain_renew, Record).
+    eepp_server:command(domain_renew, Record).
 
 domain_autorenew(Name, Autorenew) when is_boolean(Autorenew) ->
     Record = #domain_autorenew{name=Name, autorenew=atom_to_list(Autorenew)},
-    epp_server:command(domain_autorenew, Record).
+    eepp_server:command(domain_autorenew, Record).
 
 domain_transfer_request(Name) ->
     Record = #domain_transfer{name=Name, operation="request"},
-    epp_server:command(domain_transfer, Record).
+    eepp_server:command(domain_transfer, Record).
 
 domain_transfer_cancel(Name) ->
     Record = #domain_transfer{name=Name, operation="cancel"},
-    epp_server:command(domain_transfer, Record).
+    eepp_server:command(domain_transfer, Record).
 
 domain_transfer_query(Name) ->
     Record = #domain_transfer{name=Name, operation="query"},
-    epp_server:command(domain_transfer, Record).
+    eepp_server:command(domain_transfer, Record).
 
 domain_transfer_approve(Name) ->
     Record = #domain_transfer{name=Name, operation="approve"},
-    epp_server:command(domain_transfer, Record).
+    eepp_server:command(domain_transfer, Record).
 
 domain_transfer_reject(Name) ->
     Record = #domain_transfer{name=Name, operation="reject"},
-    epp_server:command(domain_transfer, Record).
+    eepp_server:command(domain_transfer, Record).
 
 poll() ->
-    epp_server:command(poll, #poll{}).
+    eepp_server:command(poll, #poll{}).
 
 ack(MessageId) ->
-    epp_server:command(ack, #ack{messageId=MessageId}).
+    eepp_server:command(ack, #ack{messageId=MessageId}).
